27ba562e05acd5d5317b20bbcd4bf899e2551b5e
cristian.mihaitactin <cristian_mihaita.ctin@yahoo.com>
2020-01-17 05:44:47 +0200

    Add Project Controller

--- private/routes.js
+++ private/routes.js
@@ -1,4 +1,6 @@
 module.exports = function(app) {
+
+  // User section
     const UserController = require('./controller/userController');
     var _userController = new UserController();
 
@@ -9,6 +11,26 @@ module.exports = function(app) {
       // .get(_userController.getAll)
   
     app.route('/users/:id')
+      .get((req, res) => {
+        _userController.get(req, res);
+      })
+      .delete((req, res) => {
+        _userController.remove(req, res);
+      });
+
+  // Project section
+    const ProjectController = require('./controller/projectController');
+    var _projectController = new ProjectController();
+
+    app.route('/users/:userid/projects')
+      .get((req, res) => {
+        _projectController.getAll(req, res);
+      })
+      .post((req, res) => {
+        _projectController.upsert(req, res);
+      });
+  
+    app.route('/users/:userid/users/:id')
       .get((req, res) => {
         _userController.get(req, res);
       })
